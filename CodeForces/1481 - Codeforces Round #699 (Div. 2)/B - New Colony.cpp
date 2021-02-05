4856d856108fa5b3f4178212f5431ec8b6a0c327
Cube219 <max804800@gmail.com>
2021-02-06 02:19:06 +0900

    CF: Upsolving 1481B - New Colony

--- CodeForces/1481 - Codeforces Round #699 (Div. 2)/B - New Colony.cpp
+++ CodeForces/1481 - Codeforces Round #699 (Div. 2)/B - New Colony.cpp
@@ -1,52 +1,49 @@
 #define _CRT_SECURE_NO_WARNINGS 1
- 
+
 #include <iostream>
 #include <cstdio>
 #include <utility>
 #include <algorithm>
 using namespace std;
- 
+
 using ll = long long int;
- 
+
 int tNum;
-ll n, k;
-ll h[1005];
-ll res;
- 
+int n, k;
+int h[105];
+int res;
+
 int main(void)
 {
     // freopen("input.txt", "r", stdin);
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
- 
+
     cin >> tNum;
     for(int tt = 1; tt <= tNum; tt++) {
         cin >> n >> k;
         for(int i = 0; i < n; ++i) {
             cin >> h[i];
         }
- 
-        while(true) {
+
+        for(int i = 0; i < k; ++i) {
             int j;
             for(j = 0; j < n - 1; ++j) {
                 if(h[j] < h[j + 1]) {
-                    k -= (h[j + 1] - h[j]);
-                    h[j] = h[j + 1];
+                    h[j]++;
                     break;
                 }
-            }
-            if(k <= 0) {
-                res = j + 1;
-                break;
             }
             if(j == n - 1) {
                 res = -1;
                 break;
+            } else if(i == k - 1) {
+                res = j + 1;
             }
         }
-        if(res == n) res = -1;
+
         cout << res << "\n";
     }
- 
+
     return 0;
 }
