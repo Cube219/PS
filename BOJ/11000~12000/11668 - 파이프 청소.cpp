223c3ceecdc9cbc96cafa0cab09c8e7e84740884
Cube219 <max804800@gmail.com>
2021-03-08 14:50:47 +0900

    BOJ: 11668 - 파이프 청소

---
+++ BOJ/11000~12000/11668 - 파이프 청소.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+using namespace std;
+
+using ll = long long int;
+
+int tNum;
+
+int main(void)
+{
+    // freopen("input.txt", "r", stdin);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> tNum;
+    for(int tt = 1; tt <= tNum; tt++) {
+    }
+
+    return 0;
+}
