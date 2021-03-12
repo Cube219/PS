e808a59fbf408092396e75d880eeaeaa1af5a575
Cube219 <max804800@gmail.com>
2021-03-12 13:40:09 +0900

    BOJ: 올바른 코드로 수정 (10327 - 피보나치 문제해결전략)

--- BOJ/10000~11000/10327 - 피보나치 문제해결전략.cpp
+++ BOJ/10000~11000/10327 - 피보나치 문제해결전략.cpp
@@ -1,55 +1,87 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS 1
+
 #include <iostream>
 #include <cstdio>
 #include <utility>
 #include <algorithm>
+#include <vector>
+#include <math.h>
 using namespace std;
-#define k(a) memset(a, 0, sizeof(a))
-using namespace std;
-const int INF = 2e9;
-const int SIZE = 1e5 + 1;
 
-int n, m;
-int arr[SIZE], tree[SIZE * 4];
-int sum[SIZE];
+using ll = long long int;
 
-int init(int left, int right, int node)
+int tNum;
+ll n;
+
+struct ENode
 {
-    if(left == right) return tree[node] = sum[left];
+    ll x;
+    ll y;
+    ll g;
+};
+ENode cal(ll a, ll b)
+{
+    if(b == 0) return { 1, 0, a };
 
-    int mid = (left + right) / 2;
-    return tree[node] = min(init(left, mid, node * 2), init(mid + 1, right, node * 2 + 1));
+    ll r = a % b;
+    auto res = cal(b, r);
+    ll x = res.y;
+    ll y = res.x - a / b * res.y;
+    return { x, y, res.g };
 }
 
-int getmin(int left, int right, int x, int y, int node)
-{
-    if(right < x || y < left) return INF;
-    if(x <= left && right <= y) return tree[node];
+ll r1, r2;
+vector<ll> f;
 
-    int mid = (left + right) / 2;
-    return min(getmin(left, mid, x, y, node * 2), getmin(mid + 1, right, x, y, node * 2 + 1));
-}
-
-int main()
+int main(void)
 {
+    // freopen("input.txt", "r", stdin);
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    //freopen("input.txt", "r", stdin);
+    cin.tie(NULL);
 
-    k(arr), k(tree), k(sum);
-
-    cin >> n >> m;
-    for(int i = 1; i <= n; i++) {
-        cin >> arr[i];
-        sum[i] = sum[i - 1] + arr[i];
+    f.push_back(1);
+    f.push_back(1);
+    int idx = 2;
+    while(1) {
+        ll t = f[idx - 1] + f[idx - 2];
+        f.push_back(t);
+        idx++;
+        if(t > 1000000000) break;
     }
-    init(0, n, 1);
 
-    int answer = 0;
-    for(int i = m; i <= n; i++)
-        answer = max(answer, sum[i] - getmin(0, n, 0, i - m, 1));
+    cin >> tNum;
+    for(int tt = 1; tt <= tNum; tt++) {
 
-    cout << answer;
+        cin >> n;
+        r1 = 1;
+        r2 = n - 1;
+
+        for(int i = 0; i < f.size() - 1; ++i) {
+            ll a = f[i];
+            ll b = f[i + 1];
+            ENode res = cal(a, b);
+            ll x = res.x * n;
+            ll y = res.y * n;
+
+            ll k = min(ceil(-(double)x / b), floor((double)y / a));
+            x += k * b;
+            y -= k * a;
+            if(x > y || x == 0) {
+                ll t = x + y;
+                x = y;
+                y = t;
+            }
+
+            if(x > 0 && y > 0) {
+                if(y < r2 || (y == r2 && x < r1)) {
+                    r1 = x;
+                    r2 = y;
+                }
+            }
+        }
+
+        cout << r1 << " " << r2 << "\n";
+    }
 
     return 0;
 }
