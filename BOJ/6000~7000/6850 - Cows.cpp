d001d5e52a1dc8c74ed8720da62d9dd850eefb6a
Cube219 <max804800@gmail.com>
2021-04-16 19:47:30 +0900

    BOJ: 6850 - Cows

---
+++ BOJ/6000~7000/6850 - Cows.cpp
@@ -0,0 +1,115 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+
+struct Point
+{
+    ll x, y;
+};
+
+Point operator+(const Point& l, const Point& r)
+{
+    return { l.x + r.x, l.y + r.y };
+}
+
+Point operator-(const Point& l, const Point& r)
+{
+    return { l.x - r.x, l.y - r.y };
+}
+
+ll dot(const Point& l, const Point& r)
+{
+    return l.x * r.x + l.y * r.y;
+}
+
+ll ccw(Point l1, Point l2)
+{
+    return l1.x * l2.y - l2.x * l1.y;
+}
+
+void convexHull(vector<Point>& p, vector<Point>& st)
+{
+    if(p.size() <= 2) {
+        st.assign(p.begin(), p.end());
+        return;
+    }
+
+    sort(p.begin(), p.end(), [](const Point& l, const Point& r) {
+        if(l.y == r.y) return l.x > r.x;
+        return l.y > r.y;
+    });
+    Point pivot = p.back();
+    p.pop_back();
+    sort(p.begin(), p.end(), [pivot](const Point& l, const Point& r) {
+        Point l1 = l - pivot;
+        Point l2 = r - pivot;
+        ll cw = ccw(l1, l2);
+        if(cw == 0) {
+            if(l1.y == l2.y) return l1.x < l2.x;
+            return l1.y < l2.y;
+        } else if(cw > 0) return true;
+        else return false;
+    });
+    st.push_back(pivot);
+    st.push_back(p[0]);
+    for(int i = 1; i < p.size(); ++i) {
+        if(st.size() < 2) {
+            st.push_back(p[i]);
+            continue;
+        }
+        Point p2 = p[i];
+        Point p1 = st[st.size() - 1];
+        Point p0 = st[st.size() - 2];
+
+        Point l1 = p1 - p0;
+        Point l2 = p2 - p0;
+        ll cw = ccw(l1, l2);
+        if(cw > 0) {
+            st.push_back(p[i]);
+        } else {
+            st.pop_back();
+            i--;
+        }
+    }
+}
+
+int n;
+vector<Point> d, d2;
+
+int main(void)
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n;
+    d.resize(n);
+    for(int i = 0; i < n; ++i) {
+        int a, b;
+        cin >> a >> b;
+        d[i] = { a, b };
+    }
+    convexHull(d, d2);
+    Point p1 = d2[0];
+    ll res = 0;
+    for(int i = 2; i < d2.size(); ++i) {
+        Point p2 = d2[i - 1];
+        Point p3 = d2[i];
+        Point v1 = p2 - p1;
+        Point v2 = p3 - p1;
+        res += abs(ccw(v1, v2));
+    }
+    cout << res / 100;
+
+    return 0;
+}
