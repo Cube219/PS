2fe47893e67c93f2d234e61b38f4ad428e44766a
Cube219 <max804800@gmail.com>
2021-05-23 12:04:56 +0900

    BOJ: 2749 - 피보나치 수 3

---
+++ BOJ/2000~3000/2749 - 피보나치 수 3.cpp
@@ -0,0 +1,109 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+constexpr ll MOD = 1000000;
+
+template <int N>
+struct Matrix
+{
+    ll d[N][N];
+
+    void init()
+    {
+        for(int i = 0; i < N; ++i) {
+            for(int j = 0; j < N; ++j) {
+                if(i == j) d[i][j] = 1;
+                else d[i][j] = 0;
+            }
+        }
+    }
+
+    void mulself()
+    {
+        Matrix tmp;
+        for(int i = 0; i < N; ++i) {
+            for(int j = 0; j < N; ++j) {
+                tmp.d[i][j] = 0;
+                for(int k = 0; k < N; ++k) {
+                    tmp.d[i][j] += (d[i][k] * d[k][j]) % MOD;
+                    tmp.d[i][j] %= MOD;
+                }
+            }
+        }
+        for(int i = 0; i < N; ++i) {
+            for(int j = 0; j < N; ++j) {
+                d[i][j] = tmp.d[i][j];
+            }
+        }
+    }
+
+    void mul(const Matrix& b)
+    {
+        Matrix tmp;
+        for(int i = 0; i < N; ++i) {
+            for(int j = 0; j < N; ++j) {
+                tmp.d[i][j] = 0;
+                for(int k = 0; k < N; ++k) {
+                    tmp.d[i][j] += (d[i][k] * b.d[k][j]) % MOD;
+                    tmp.d[i][j] %= MOD;
+                }
+            }
+        }
+        for(int i = 0; i < N; ++i) {
+            for(int j = 0; j < N; ++j) {
+                d[i][j] = tmp.d[i][j];
+            }
+        }
+    }
+
+    void pow(ll p)
+    {
+        Matrix res;
+        res.init();
+        while(p > 0) {
+            if(p % 2 == 1) {
+                res.mul(*this);
+            }
+            mulself();
+            p /= 2;
+        }
+        for(int i = 0; i < N; ++i) {
+            for(int j = 0; j < N; ++j) {
+                d[i][j] = res.d[i][j];
+            }
+        }
+    }
+};
+
+ll n;
+Matrix<2> m;
+
+int main(void)
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n;
+    m.d[0][0] = 1;
+    m.d[0][1] = 1;
+    m.d[1][0] = 1;
+    m.d[1][1] = 0;
+    m.pow(n - 2);
+
+    if(n == 0) cout << "0";
+    else if(n == 1) cout << "1";
+    else cout << (m.d[0][0] + m.d[0][1]) % MOD;
+
+    return 0;
+}
