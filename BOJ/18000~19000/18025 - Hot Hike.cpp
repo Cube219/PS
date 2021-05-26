2852c88729216663747c00609b1236c1d775c89d
Cube219 <max804800@gmail.com>
2021-05-26 15:04:50 +0900

    BOJ: 18025 - Hot Hike

---
+++ BOJ/18000~19000/18025 - Hot Hike.cpp
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+
+int n;
+int d[51];
+int res = 987654321;
+int ridx;
+
+int main(void)
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n;
+    for(int i = 0; i < n; ++i) {
+        cin >> d[i];
+    }
+    for(int i = 0; i < n - 2; ++i) {
+        int cur = max(d[i], d[i + 2]);
+        if(res > cur) {
+            res = cur;
+            ridx = i;
+        }
+    }
+    cout << ridx + 1 << " " << res;
+
+    return 0;
+}
