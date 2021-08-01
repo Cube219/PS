0fbdb5461ed53f6c5e70fb2ffc00ebad3708c02d
Cube219 <max804800@gmail.com>
2021-08-01 11:17:07 +0900

    BOJ: 22357 - 경품 추첨

---
+++ BOJ/22000~23000/22357 - 경품 추첨.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+
+int n, k;
+int d[] = { 2003, 2011, 2017, 2027, 2029, 2039, 2053, 2063, 2069, 2081, 2083, 2087, 2089, 2099, 2111, 2113, 2129, 2131, 2137, 2141, 2143, 2153, 2161, 2179, 2203, 2207, 2213, 2221, 2237, 2239 };
+
+int main(void)
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> k >> n;
+    for(int i = 0; i < k; ++i) {
+        int cur = 1;
+        for(int j = 0; j < n; ++j) {
+            cout << cur << " ";
+            cur += d[i];
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
