61b9060fbf4e7180c954c2d39eebef9a7f3ebc15
Cube219 <max804800@gmail.com>
2021-11-07 23:58:31 +0900

    BOJ: 18301 - Rats

---
+++ BOJ/18000~19000/18301 - Rats.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+
+ll n1, n2, n12;
+
+int main(void)
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n1 >> n2 >> n12;
+    cout << (n1 + 1) * (n2 + 1) / (n12 + 1) - 1;
+
+    return 0;
+}
