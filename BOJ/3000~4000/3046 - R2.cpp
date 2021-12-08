3eda33caf08e4038e6da6ad2774e247b286f82f6
Cube219 <max804800@gmail.com>
2021-12-09 00:03:46 +0900

    BOJ: 3046 - R2

---
+++ BOJ/3000~4000/3046 - R2.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+
+int main(void)
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int r1, s;
+    cin >> r1 >> s;
+    cout << s * 2 - r1;
+
+    return 0;
+}
