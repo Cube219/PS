4d0e7ce066f1ea7f48111bee67fefde15927ea20
Cube219 <max804800@gmail.com>
2021-12-10 23:25:00 +0900

    BOJ: 5520 - 카드 게임

---
+++ BOJ/5000~6000/5520 - 카드 게임.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+
+int main(void)
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int res = 0;
+    for(int i = 0; i < 5; ++i) {
+        int a;
+        cin >> a;
+        res += a;
+    }
+    cout << res;
+
+    return 0;
+}
