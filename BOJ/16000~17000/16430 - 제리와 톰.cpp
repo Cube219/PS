949d8b2e71ec7587a652632ffc27e24bfb24773c
Cube219 <max804800@gmail.com>
2021-12-12 01:29:58 +0900

    BOJ: 16430 - 제리와 톰

---
+++ BOJ/16000~17000/16430 - 제리와 톰.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+
+int tNum;
+
+int main(void)
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a, b;
+    cin >> a >> b;
+    cout << (b - a) << " " << b;
+
+    return 0;
+}
