5e1e77e4d149312f863d1e0192df27f13441b9ca
Cube219 <max804800@gmail.com>
2021-12-13 02:28:10 +0900

    BOJ: 23825 - SASA 모형을 만들어보자

---
+++ BOJ/23000~24000/23825 - SASA 모형을 만들어보자.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+
+int main(void)
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+    cout << min(n / 2, m / 2);
+
+    return 0;
+}
