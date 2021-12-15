8cbfebd03325c4e9ae5dac2160c2aa1fb74b4686
Cube219 <max804800@gmail.com>
2021-12-16 00:53:14 +0900

    BOJ: 2845 - 파티가 끝나고 난 뒤

---
+++ BOJ/2000~3000/2845 - 파티가 끝나고 난 뒤.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LNF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MOD = 1'000'000'007;
+
+int main(void)
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll l, p;
+    cin >> l >> p;
+    p *= l;
+    for(int i = 0; i < 5; ++i) {
+        ll v;
+        cin >> v;
+        cout << v - p << " ";
+    }
+
+    return 0;
+}
