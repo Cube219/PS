1066473e205edebfaf81cbd89150c4213264f85c
Cube219 <max804800@gmail.com>
2021-12-17 01:01:17 +0900

    BOJ: 15894 - 수학은 체육과목 입니다

---
+++ BOJ/15000~16000/15894 - 수학은 체육과목 입니다.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LNF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MOD = 1'000'000'007;
+
+int main(void)
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n;
+    cin >> n;
+    cout << n * 4;
+
+    return 0;
+}
