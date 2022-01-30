518a7e24a6d5482ddad7c1770e8b6ac9500c30a8
Cube219 <max804800@gmail.com>
2022-01-30 21:14:37 +0900

    BOJ: 24392 - 영재의 징검다리

---
+++ BOJ/24000~25000/24392 - 영재의 징검다리.cpp
@@ -0,0 +1,129 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LNF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MOD = 1'000'000'007;
+
+template <int mod>
+struct ModInt
+{
+    ll v;
+    ModInt(ll _v = 0) : v((_v % mod + mod) % mod) {}
+    ModInt& operator+=(const ModInt& rhs) {
+        v += rhs.v;
+        if(v >= mod) v -= mod;
+        return *this;
+    }
+    ModInt& operator-=(const ModInt& rhs) {
+        v -= rhs.v;
+        if(v < 0) v += mod;
+        return *this;
+    }
+    ModInt& operator*=(const ModInt& rhs) {
+        v *= rhs.v;
+        v %= mod;
+        return *this;
+    }
+    ModInt& operator/=(const ModInt& rhs) {
+        ll n = mod - 2;
+        ModInt rev = 1, b = rhs;
+        while(n > 0) {
+            if(n & 1) rev *= b;
+            b *= b;
+            n >>= 1;
+        }
+        return *this *= rev;
+    }
+    ModInt operator+(const ModInt& rhs) const {
+        ModInt lhs(*this);
+        return lhs += rhs;
+    }
+    ModInt operator-() const { return ModInt() -= *this; }
+    ModInt operator-(const ModInt& rhs) const {
+        ModInt lhs(*this);
+        return lhs -= rhs;
+    }
+    ModInt operator*(const ModInt& rhs) const {
+        ModInt lhs(*this);
+        return lhs *= rhs;
+    }
+    ModInt operator/(const ModInt& rhs) const {
+        ModInt lhs(*this);
+        return lhs /= rhs;
+    }
+    friend std::ostream& operator<<(std::ostream& os, const ModInt& n) { return os << n.v; }
+    friend std::istream& operator>>(std::istream& is, ModInt& n) {
+        ll x;
+        is >> x;
+        n = ModInt(x);
+        return is;
+    }
+    bool operator==(const ModInt& rhs) const { return v == rhs.v; }
+    bool operator!=(const ModInt& rhs) const { return v != rhs.v; }
+    ModInt pow(ll k) const {
+        ModInt res = 1, p = v;
+        while(k > 0) {
+            if(k & 1) res *= p;
+            p *= p;
+            k >>= 1;
+        }
+        return res;
+    }
+};
+using mint = ModInt<MOD>;
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> map(n, vector<int>(m));
+    for(auto& v1 : map) {
+        for(auto& v2 : v1) cin >> v2;
+    }
+
+    vector<vector<mint>> dp(n, vector<mint>(m, 0));
+    for(int i = 0; i < m; ++i) {
+        if(map[n - 1][i] == 1) dp[n - 1][i] = 1;
+    }
+    for(int i = n - 2; i >= 0; --i) {
+        for(int j = 0; j < m; ++j) {
+            if(map[i][j] == 0) continue;
+            for(int k = -1; k <= 1; ++k) {
+                if((j + k) < 0 || (j + k) >= m) continue;
+                dp[i][j] += dp[i + 1][j + k];
+            }
+        }
+    }
+
+    mint res = 0;
+    for(int i = 0; i < m; ++i) {
+        res += dp[0][i];
+    }
+    cout << res;
+}
+
+int main()
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tNum = 1;
+    // cin >> tNum;
+    for(int tt = 1; tt <= tNum; tt++) {
+        solve();
+    }
+
+    return 0;
+}
