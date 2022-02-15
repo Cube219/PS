3a36a3f1637c439e08b72f78bdea2ccbdfe514fd
Cube219 <max804800@gmail.com>
2022-02-16 01:12:07 +0900

    BOJ: 5596 - 시험 점수

---
+++ BOJ/5000~6000/5596 - 시험 점수.cpp
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LNF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MOD = 1'000'000'007;
+
+void solve()
+{
+    int res = 0;
+    int sum = 0;
+    for(int i = 0; i < 4; ++i) {
+        int a;
+        cin >> a;
+        sum += a;
+    }
+    res = sum;
+    sum = 0;
+    for(int i = 0; i < 4; ++i) {
+        int a;
+        cin >> a;
+        sum += a;
+    }
+    res = max(res, sum);
+    cout << res;
+}
+
+int main()
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tNum = 1;
+    // cin >> tNum;
+    for(int tt = 1; tt <= tNum; tt++) {
+        solve();
+    }
+
+    return 0;
+}
