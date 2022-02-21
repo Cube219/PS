9cdd7840892059a63f1f9cadb81c5c6662ff03c1
Cube219 <max804800@gmail.com>
2022-02-22 00:33:50 +0900

    BOJ: 24505 - blobhyperlink

---
+++ BOJ/24000~25000/24505 - blobhyperlink.cpp
@@ -0,0 +1,204 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LNF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MOD = 1'000'000'007;
+
+template <int mod>
+struct ModInt
+{
+    ll v;
+    ModInt(ll _v = 0) : v((_v % mod + mod) % mod) {}
+    ModInt& operator+=(const ModInt& rhs) {
+        v += rhs.v;
+        if(v >= mod) v -= mod;
+        return *this;
+    }
+    ModInt& operator-=(const ModInt& rhs) {
+        v -= rhs.v;
+        if(v < 0) v += mod;
+        return *this;
+    }
+    ModInt& operator*=(const ModInt& rhs) {
+        v *= rhs.v;
+        v %= mod;
+        return *this;
+    }
+    ModInt& operator/=(const ModInt& rhs) {
+        ll n = mod - 2;
+        ModInt rev = 1, b = rhs;
+        while(n > 0) {
+            if(n & 1) rev *= b;
+            b *= b;
+            n >>= 1;
+        }
+        return *this *= rev;
+    }
+    ModInt operator+(const ModInt& rhs) const {
+        ModInt lhs(*this);
+        return lhs += rhs;
+    }
+    ModInt operator-() const { return ModInt() -= *this; }
+    ModInt operator-(const ModInt& rhs) const {
+        ModInt lhs(*this);
+        return lhs -= rhs;
+    }
+    ModInt operator*(const ModInt& rhs) const {
+        ModInt lhs(*this);
+        return lhs *= rhs;
+    }
+    ModInt operator/(const ModInt& rhs) const {
+        ModInt lhs(*this);
+        return lhs /= rhs;
+    }
+    friend std::ostream& operator<<(std::ostream& os, const ModInt& n) { return os << n.v; }
+    friend std::istream& operator>>(std::istream& is, ModInt& n) {
+        ll x;
+        is >> x;
+        n = ModInt(x);
+        return is;
+    }
+    bool operator==(const ModInt& rhs) const { return v == rhs.v; }
+    bool operator!=(const ModInt& rhs) const { return v != rhs.v; }
+    ModInt pow(ll k) const {
+        ModInt res = 1, p = v;
+        while(k > 0) {
+            if(k & 1) res *= p;
+            p *= p;
+            k >>= 1;
+        }
+        return res;
+    }
+};
+using mint = ModInt<MOD>;
+
+struct SegTree
+{
+    int n;
+    vector<mint> t;
+
+    SegTree(int _n)
+    {
+        n = _n;
+        t.resize(n * 2, emptyNode);
+    }
+
+    void initv(int idx, mint v)
+    {
+        t[n + idx] = v;
+    }
+
+    void clear()
+    {
+        for(int i = 0; i < n * 2; ++i) {
+            t[i] = 0;
+        }
+    }
+
+    void build()
+    {
+        for(int i = n - 1; i > 0; --i) {
+            t[i] = merge(t[i << 1], t[i << 1 | 1]);
+        }
+    }
+
+    void update(int idx, mint v)
+    {
+        idx += n;
+        t[idx] = v;
+        idx >>= 1;
+        while(idx > 0) {
+            t[idx] = merge(t[idx << 1], t[idx << 1 | 1]);
+            idx >>= 1;
+        }
+    }
+
+    mint find(int l, int r)
+    {
+        l += n;
+        r += n + 1;
+        mint resl = emptyNode;
+        mint resr = emptyNode;
+        while(l < r) {
+            if(l & 1) resl = merge(resl, t[l++]);
+            if(r & 1) resr = merge(t[--r], resr);
+            l >>= 1;
+            r >>= 1;
+        }
+        return merge(resl, resr);
+    }
+
+    mint merge(mint l, mint r)
+    {
+        return l + r;
+    }
+
+    const mint emptyNode = 0;
+};
+
+
+void solve()
+{
+    int n;
+    cin >> n;
+    struct Node
+    {
+        int idx, v;
+    };
+    vector<Node> d(n);
+    for(int i = 0; i < n; ++i) {
+        cin >> d[i].v;
+        d[i].idx = i;
+    }
+    sort(d.begin(), d.end(), [](const Node& l, const Node& r) {
+        if(l.v == r.v) return l.idx > r.idx;
+        return l.v < r.v;
+    });
+
+    vector<mint> dp(n, 1);
+    for(int zz = 1; zz < 11; ++zz) {
+        SegTree sg(n);
+        vector<mint> nxt(n, 0);
+
+        for(int i = 0; i < n; ++i) {
+            auto [idx, v] = d[i];
+            nxt[idx] = sg.find(0, idx - 1);
+
+            sg.update(idx, dp[idx]);
+        }
+
+        dp = move(nxt);
+    }
+    mint res = 0;
+    for(int i = 0; i < n; ++i) {
+        res += dp[i];
+    }
+    cout << res;
+}
+
+int main()
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tNum = 1;
+    // cin >> tNum;
+    for(int tt = 1; tt <= tNum; tt++) {
+        solve();
+    }
+
+    return 0;
+}
