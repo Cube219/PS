7bc12516df7024d95652a301f40dcb12b08919b6
Cube219 <max804800@gmail.com>
2022-02-22 00:33:05 +0900

    BOJ: 24500 - blobblush

---
+++ BOJ/24000~25000/24500 - blobblush.cpp
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LNF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MOD = 1'000'000'007;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    int msb = 0;
+    for(ll tmp = n; tmp > 0; tmp >>= 1, msb++);
+
+    ll mask = (1LL << (msb)) - 1;
+    ll b = (~n) & mask;
+    if(b == 0) {
+        cout << "1\n";
+        cout << n;
+    } else {
+        cout << "2\n";
+        cout << b << "\n";
+        cout << n << "\n";
+    }
+}
+
+int main()
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tNum = 1;
+    // cin >> tNum;
+    for(int tt = 1; tt <= tNum; tt++) {
+        solve();
+    }
+
+    return 0;
+}
