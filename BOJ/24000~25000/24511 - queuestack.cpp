c0b867b1869bf6249e986a10edac73b6417481b3
Cube219 <max804800@gmail.com>
2022-02-25 02:13:12 +0900

    BOJ: 24511 - queuestack

---
+++ BOJ/24000~25000/24511 - queuestack.cpp
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <numeric>
+#include <queue>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LNF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MOD = 1'000'000'007;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> d(n), d2(n);
+    for(auto& v : d) cin >> v;
+    for(auto& v : d2) cin >> v;
+    queue<int> q;
+    for(int i = n - 1; i >= 0; --i) {
+        if(d[i] == 1) continue;
+        q.push(d2[i]);
+    }
+
+    int m;
+    cin >> m;
+    for(int i = 0; i < m; ++i) {
+        int a;
+        cin >> a;
+        q.push(a);
+        cout << q.front() << " ";
+        q.pop();
+    }
+}
+
+int main()
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tNum = 1;
+    // cin >> tNum;
+    for(int tt = 1; tt <= tNum; tt++) {
+        solve();
+    }
+
+    return 0;
+}
