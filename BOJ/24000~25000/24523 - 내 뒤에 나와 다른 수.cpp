679dfb60f99497f0e0b20a7120b66d5ee5aecddf
Cube219 <max804800@gmail.com>
2022-03-02 01:45:19 +0900

    BOJ: 24523 - 내 뒤에 나와 다른 수

---
+++ BOJ/24000~25000/24523 - 내 뒤에 나와 다른 수.cpp
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LNF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MOD = 1'000'000'007;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> d(n);
+    for(auto& v : d) cin >> v;
+
+    vector<int> res(n);
+    res[n - 1] = -1;
+    for(int i = n - 2; i >= 0; --i) {
+        if(d[i] == d[i + 1]) res[i] = res[i + 1];
+        else res[i] = i + 2;
+    }
+
+    for(int v : res) cout << v << " ";
+}
+
+int main()
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tNum = 1;
+    // cin >> tNum;
+    for(int tt = 1; tt <= tNum; tt++) {
+        solve();
+    }
+
+    return 0;
+}
