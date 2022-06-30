09c693feaced81b674a976fab44b851076bfad41
Cube219 <max804800@gmail.com>
2022-06-30 20:35:45 +0900

    BOJ: 23337 - Drunk Passenger

---
+++ BOJ/23000~24000/23337 - Drunk Passenger.cpp
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <iomanip>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LNF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MOD = 1'000'000'007;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<double> dp(n, 0);
+    dp[1] = 1.0 / (n - 1);
+
+    for(int i = 2; i < n; ++i) {
+        dp[i] = 1.0 / (n - 1);
+        for(int j = 1; j < i; ++j) {
+            dp[i] += dp[j] / (n - j);
+        }
+    }
+    cout << fixed << setprecision(10) << dp[n - 1];
+}
+
+int main()
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tNum = 1;
+    // cin >> tNum;
+    for(int tt = 1; tt <= tNum; tt++) {
+        solve();
+    }
+
+    return 0;
+}
