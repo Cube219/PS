69acdbfb0bab1562e49460326c502bc847db1528
Cube219 <max804800@gmail.com>
2022-07-13 17:09:28 +0900

    Lib: Fix hilbert curve bug

--- Library/Math/Hilbert Curve (pshilbertcurve).cpp
+++ Library/Math/Hilbert Curve (pshilbertcurve).cpp
@@ -1,21 +1,20 @@
 // https://en.wikipedia.org/wiki/Hilbert_curve
 //convert (x,y) to d
-int hilbertCurve(int x, int y, int n = (1 << 21))
+ll hilbertCurve(int x, int y, ll n)
 {
-    n = (1 << (n - 1));
-    int rx, ry, s, d = 0;
+    ll rx, ry, s = 1, d = 0;
+    while(s < n) s *= 2;
+    n = s;
     for(s = n / 2; s > 0; s /= 2) {
         rx = (x & s) > 0;
         ry = (y & s) > 0;
         d += s * s * ((3 * rx) ^ ry);
-
         // rotate
         if(ry == 0) {
             if(rx == 1) {
                 x = n - 1 - x;
                 y = n - 1 - y;
             }
-
             swap(x, y);
         }
     }
