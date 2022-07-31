bda8bbb6cb9dfe0198577ee302549eb5e6a8b2e7
Cube219 <max804800@gmail.com>
2022-07-31 18:31:33 +0900

    BOJ: 23138 - Swapping Inversions

---
+++ BOJ/23000~24000/23138 - Swapping Inversions.cpp
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LNF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MOD = 1'000'000'007;
+
+struct Fenwick
+{
+    int n;
+    vector<ll> t;
+    Fenwick(int _n) : n(_n + 1), t(_n + 1, 0) {}
+
+    void update(int pos, ll dv)
+    {
+        pos++;
+        for(; pos < n; pos += (pos & -pos)) t[pos] += dv;
+    }
+
+    ll query(int pos)
+    {
+        pos++;
+        ll res = 0;
+        for(; pos > 0; pos -= (pos & -pos)) res += t[pos];
+        return res;
+    }
+};
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> d(n);
+    for(auto& v : d) {
+        cin >> v;
+        v--;
+    }
+
+    Fenwick fwcnt(n), fwsum(n);
+    ll res = 0;
+    for(int i = n - 1; i >= 0; --i) {
+        ll cnt = fwcnt.query(d[i]);
+        ll sum = fwsum.query(d[i]);
+
+        res = (res + (d[i] * cnt - sum)) % MOD;
+        fwcnt.update(d[i], 1);
+        fwsum.update(d[i], d[i]);
+    }
+
+    cout << res;
+}
+
+int main()
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tNum = 1;
+    // cin >> tNum;
+    for(int tt = 1; tt <= tNum; tt++) {
+        solve();
+    }
+
+    return 0;
+}
