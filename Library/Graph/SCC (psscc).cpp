59c59db046f219c6bd699fb2c40fe1db6cd87402
Cube219 <max804800@gmail.com>
2022-08-12 23:12:14 +0900

    Lib: Change variable name / Fixed wrong sccIdx in SCC

--- Library/Graph/SCC (psscc).cpp
+++ Library/Graph/SCC (psscc).cpp
@@ -1,18 +1,18 @@
-vector<int> d(n, 0), st, finish(n, false), sccIdx(n);
+vector<int> dep(n, 0), st, finish(n, false), sccIdx(n);
 vector<vector<int>> sccs;
 int dNum = 0;
 
 auto scc_dfs = [&](auto&& self, int cur) -> int {
-    d[cur] = ++dNum;
+    dep[cur] = ++dNum;
     st.push_back(cur);
 
-    int ret = d[cur];
+    int ret = dep[cur];
     for(int nxt : g[cur]) {
-        if(d[nxt] == 0) ret = min(ret, self(self, nxt));
-        else if(finish[nxt] == false) ret = min(ret, d[nxt]);
+        if(dep[nxt] == 0) ret = min(ret, self(self, nxt));
+        else if(finish[nxt] == false) ret = min(ret, dep[nxt]);
     }
 
-    if(ret == d[cur]) {
+    if(ret == dep[cur]) {
         sccs.push_back({});
         auto& scc = sccs.back();
         while(1) {
@@ -20,7 +20,7 @@ auto scc_dfs = [&](auto&& self, int cur) -> int {
             st.pop_back();
 
             scc.push_back(top);
-            sccIdx[top] = sccs.size();
+            sccIdx[top] = (int)sccs.size() - 1;
             finish[top] = true;
 
             if(top == cur) break;
@@ -29,5 +29,5 @@ auto scc_dfs = [&](auto&& self, int cur) -> int {
     return ret;
 };
 for(int i = 0; i < n; ++i) {
-    if(d[i] == 0) scc_dfs(scc_dfs, i);
+    if(dep[i] == 0) scc_dfs(scc_dfs, i);
 }
