551aaa6c0b0f5a182958a493a919d529fac6e1ec
Cube219 <max804800@gmail.com>
2022-08-24 02:02:05 +0900

    BOJ: 25495 - 에어팟

---
+++ BOJ/25000~26000/25495 - 에어팟.cpp
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LNF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MOD = 1'000'000'007;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    ll cur = 100, last = -1, use = 0;
+    for(int i = 0; i < n; ++i) {
+        int a;
+        cin >> a;
+        if(last == a) {
+            use *= 2;
+        } else {
+            use = 2;
+        }
+        cur -= use;
+        last = a;
+        if(cur <= 0) {
+            last = -1;
+            cur = 100;
+        }
+    }
+    
+    cout << 100 - cur;
+}
+
+int main()
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tNum = 1;
+    // cin >> tNum;
+    for(int tt = 1; tt <= tNum; tt++) {
+        solve();
+    }
+
+    return 0;
+}
