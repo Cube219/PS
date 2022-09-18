c073629fd5544a56079db8fbacec6a48453645e2
Cube219 <max804800@gmail.com>
2022-09-18 23:44:37 +0900

    BOJ: 25600 - Triathlon

---
+++ BOJ/25000~26000/25600 - Triathlon.cpp
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LNF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MOD = 1'000'000'007;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    struct Node { ll a, d, g; };
+    vector<Node> d(n);
+    ll res = 0;
+    for(int i = 0; i < n; ++i) {
+        ll a, d, g;
+        cin >> a >> d >> g;
+        ll v = a * (d + g);
+        if(a == d + g) v *= 2;
+        res = max(res, v);
+    }
+
+    cout << res;
+}
+
+int main()
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tNum = 1;
+    // cin >> tNum;
+    for(int tt = 1; tt <= tNum; tt++) {
+        solve();
+    }
+
+    return 0;
+}
