f20619e0a81de8e5b1d2407a2e56ebd77b2f2c45
Cube219 <max804800@gmail.com>
2022-09-23 11:05:25 +0000

    BOJ: 25592 - 바둑돌 게임

---
+++ BOJ/25000~26000/25592 - 바둑돌 게임.cpp
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LNF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MOD = 1'000'000'007;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int turn = 0;
+    int i;
+    for(i = 1; n - i >= 0; ++i) {
+        n -= i;
+        turn = 1 - turn;
+    }
+
+    if(turn == 1) cout << "0";
+    else
+        cout << i - n;
+}
+
+int main()
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tNum = 1;
+    // cin >> tNum;
+    for(int tt = 1; tt <= tNum; tt++) {
+        solve();
+    }
+
+    return 0;
+}
