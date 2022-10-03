fc6898b5766225454f1bcdfa5b0ab82b94e04773
Cube219 <max804800@gmail.com>
2022-10-04 05:07:36 +0900

    BOJ: 25703 - 포인터 공부

---
+++ BOJ/25000~26000/25703 - 포인터 공부.cpp
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LNF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MOD = 1'000'000'007;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    cout << "int a;\n";
+    cout << "int *ptr = &a;\n";
+    if(n >= 2) cout << "int **ptr2 = &ptr;\n";
+
+    for(int i = 3; i <= n; ++i) {
+        cout << "int ";
+        for(int j = 0; j < i; ++j) {
+            cout << "*";
+        }
+        cout << "ptr" << i << " = &ptr" << i - 1 << ";\n";
+    }
+}
+
+int main()
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tNum = 1;
+    // cin >> tNum;
+    for(int tt = 1; tt <= tNum; tt++) {
+        solve();
+    }
+
+    return 0;
+}
