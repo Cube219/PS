aed5c5e0b2391a136ebae07dbe6c8c65136db87b
Cube219 <max804800@gmail.com>
2022-10-31 17:22:36 +0900

    BOJ: 25905 - 장인은 도구를 탓하지 않는다

---
+++ BOJ/25000~26000/25905 - 장인은 도구를 탓하지 않는다.cpp
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_DECLARE_NONSTDC_NAMES 0 // for using y1 as variable name
+
+#include <iostream>
+#include <cstdio>
+#include <utility>
+#include <algorithm>
+#include <iomanip>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+using ll = long long int;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LNF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MOD = 1'000'000'007;
+
+void solve()
+{
+    vector<double> d(10);
+    for(auto& v : d) cin >> v;
+
+    sort(d.begin(), d.end());
+    float res = 1;
+    for(int i = 1; i <= 9; ++i) {
+        res *= d.back() / i * 10;
+        d.pop_back();
+    }
+    cout << fixed << setprecision(15) << res;
+}
+
+int main()
+{
+#ifdef CUBE_PS
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tNum = 1;
+    // cin >> tNum;
+    for(int tt = 1; tt <= tNum; tt++) {
+        solve();
+    }
+
+    return 0;
+}
